e60ea7929298f6615d0f431fdbef70bde3040f54
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-07-20 00:21:18 +0530

    basic structure created

---
+++ healthylifeadvice/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Heading,Box,Image,Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box className="App">
+      
+      
+
+      <Box id=''>
+      <Heading>Healthy Life Advice</Heading>
+      <Text>I will create a personalized meditation for you.Then I will guide you through it!</Text>
+      {/* <Image src="./Images/HealthAdvice2.svg" alt='HealthAdvice'></Image> */}
+      </Box>
+    </Box>
+  );
+}
+
+export default App;
